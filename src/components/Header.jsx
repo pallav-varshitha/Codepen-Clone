1f9626b9f01b4c792333a58f663812ddaf1a9dff
pallav-varshitha <103641244+pallav-varshitha@users.noreply.github.com>
2023-07-10 15:29:33 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+
+import { AppBar, Toolbar, styled } from '@mui/material';
+
+const Container = styled(AppBar)`
+    background:#060606;
+    height: 9vh;
+`
+
+
+const Header = () => {
+     
+    return (
+        <Container position='static'>
+            <Toolbar>
+                
+            </Toolbar>
+        </Container>
+        );
+    
+
+}
+
+export default Header;
